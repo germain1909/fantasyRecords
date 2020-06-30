d4571f7473fb012f668bffac63add04f6bb3880a
Germain Burchfield <germainburchfield@Germains-iMac.attlocal.net>
2020-06-29 20:34:13 -0400

    initial baseline

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+{/* <View style={styles.container}>
+        <Text>Get money G  Dont quit Get paid</Text>
+        <StatusBar style="auto" />
+      </View> */}
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Roster" component={HomeScreen} />
+        <Tab.Screen name="Showcase" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
